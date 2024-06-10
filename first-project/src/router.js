f1ae89267ec89769931ee75f88de437ea34808f8
egrizq <rizq.ramadhan17@gmail.com>
2024-06-10 09:41:50 +0700

    fix: database router

--- first-project/src/router.js
+++ first-project/src/router.js
@@ -2,10 +2,12 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 
 import Page from './Page.vue'
 import About from './About.vue'
+import Database from './Database.vue'
 
 const routes = [
   { path: '/', component: Page },
   { path: '/about', component: About },
+  { path: '/database', component: Database }
 ]
 
 const router = createRouter({
