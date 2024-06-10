38498aedaac061fcfac81ba9d8c73de46eb57504
egrizq <rizq.ramadhan17@gmail.com>
2024-06-10 09:45:19 +0700

    feat: connect to database

---
+++ first-project/src/lib/supabaseClient.js
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://mytwbdfafmlubmuhmdvf.supabase.co', 
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15dHdiZGZhZm1sdWJtdWhtZHZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2NTA2MDMsImV4cCI6MjAyODIyNjYwM30.TTCkQETBuCw1ZuUVtlgTUGeKbNE_OXtrEEMX0I4fgeE')
